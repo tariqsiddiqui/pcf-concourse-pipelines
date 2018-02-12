f9e5637c94bbae7c7aeb449f94f50ee04fe88df3
Tariq Siddiqui <tariqsiddiqui@comcast.net>
2018-02-12 11:52:34 -0500

    sdfsd

--- tasks/config-spring-cloud-services/task.sh
+++ tasks/config-spring-cloud-services/task.sh
@@ -44,12 +44,17 @@ PRODUCT_NETWORK=$(
     --arg network_name "$NETWORK_NAME" \
     '. +
     {
+      "singleton_availability_zone": {
+        "name": $singleton_jobs_az
+      },
+      "other_availability_zones": ($other_azs | split(",") | map({name: .})),
       "network": {
         "name": $network_name
       }
     }
     '
 )
+
 
 echo "Testing"
 echo $PRODUCT_NETWORK
